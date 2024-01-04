6add561cd672f7aef546cf3379ca76fdde420ffd
generalrowland <113939475+generalrowland@users.noreply.github.com>
2024-01-04 15:15:05 -0600

    er

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public int monstersToSpawn;
+    public Transform[] spawnPoints;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
