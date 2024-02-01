4dd145753038f91de553e4cefd58e5a14b2defe4
generalrowland <113939475+generalrowland@users.noreply.github.com>
2024-02-01 13:40:50 -0600

    jh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -27,6 +29,7 @@ public class Player : MonoBehaviour
         {
             currentHealth -= damage;
 
+            healthBar.SetHealth(currentHealth);
         }
     }
 }
