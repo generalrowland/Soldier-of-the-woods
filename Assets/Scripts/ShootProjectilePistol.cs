ab8aded3e5521affd49f19254b5cf0e25e327fde
generalrowland <113939475+generalrowland@users.noreply.github.com>
2024-02-08 14:50:18 -0600

    cv

--- Assets/Scripts/ShootProjectilePistol.cs
+++ Assets/Scripts/ShootProjectilePistol.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class ShootProjectilePistol : MonoBehaviour
 {
+    public Transform firePosition;
+    private bool isFiring;
+    public float launchVelocity = 50f;
+    public GameObject Projectile;
+    public bool canFire;
+    private float fireTime = 0f;
+    private float fireRate = 1f;
+    protected Vector3 launchVector;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        launchVector = new Vector3(0, 0, launchVelocity);
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fireTime -= Time.deltaTime;
+
+
+        if (fireTime <= 0)
+        {
+            canFire = true;
+            fireTime = 0;
+        }
+        else
+        {
+            canFire = false;
+        }
+
+        if (Input.GetMouseButtonDown(0) & canFire)
+        {
+            Invoke("Fire", 0.35f);
+
+            animator.SetTrigger("IsAttacking");
+        }
+    }
+
+    void Fire()
+    {
+        GameObject clone = Instantiate(Projectile, firePosition.position, firePosition.rotation);
+        clone.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity));
+        fireTime = fireRate;
+
     }
 }
