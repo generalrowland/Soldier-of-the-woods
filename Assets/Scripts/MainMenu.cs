6742789b5789198e43fdda4701d94e584b025db3
generalrowland <113939475+generalrowland@users.noreply.github.com>
2024-03-01 14:37:53 -0600

    vm

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -24,4 +24,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void GoToCreditsMenu()
+    {
+        SceneManager.LoadScene("CreditsMenu");
+    }
 }
