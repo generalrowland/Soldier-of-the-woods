c807797dfcbf07af0b6ce2a4c7e8f4ddcbc5fb51
generalrowland <113939475+generalrowland@users.noreply.github.com>
2024-03-07 15:13:58 -0600

    hjhg

---
+++ Assets/Scripts/PlayerFootSteps.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootSteps : MonoBehaviour
+{
+    public AudioClip footSteps;
+    public AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    public void PlayFootSteps ()
+    {
+        audioSource.PlayOneShot (footSteps);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
