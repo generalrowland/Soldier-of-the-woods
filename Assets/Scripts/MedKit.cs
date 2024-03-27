df58936f5fab6c207af1aed45ee3aecf3d553f97
generalrowland <113939475+generalrowland@users.noreply.github.com>
2024-03-27 14:09:08 -0500

    bh

--- Assets/Scripts/MedKit.cs
+++ Assets/Scripts/MedKit.cs
@@ -6,6 +6,8 @@ public class MedKit : MonoBehaviour
 {
 
     public int healAmount;
+    public int duration;
+    private float currentHealth;
 
 
     private void OnTriggerEnter(Collider other)
@@ -17,4 +19,20 @@ public class MedKit : MonoBehaviour
         }
     }
 
+    public void HealOverTime(int healAmount, int duration)
+    {
+        StartCoroutine(HealOverTimeCoroutine(healAmount, duration));
+    }
+
+    IEnumerator HealOverTimeCoroutine(float healAmount, float duration)
+    {
+        float amountHealed = 10;
+        float healPerLoop = healAmount / duration;
+        while (amountHealed < healAmount)
+        {
+            currentHealth += healPerLoop;
+            amountHealed += healPerLoop;
+            yield return new WaitForSeconds(1f);
+        }
+    }
 }
